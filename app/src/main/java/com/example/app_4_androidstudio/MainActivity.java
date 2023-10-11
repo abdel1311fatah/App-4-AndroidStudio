413398b6c6605eae1fbf3081c27477f4891c8fce
aibarra-vegga <129413159+abdel1311fatah@users.noreply.github.com>
2023-10-11 10:04:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/app_4_androidstudio/MainActivity.java
@@ -0,0 +1,16 @@
+//Abel Ibarra
+
+package com.example.app_4_androidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
